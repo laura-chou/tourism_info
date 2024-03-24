fa3a71fafe284a368f762ab80fe5053f79904a25
Yu-Lun <chou522128@gmail.com>
2024-03-24 17:41:50 +0800

    revise html

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { useStore } from '@/stores/index.js'
 import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 const store = useStore()
 onMounted(() => {
   window.addEventListener('beforeunload', () => {
     store.isReload = true
+    sessionStorage.redirect = store.getDataPath(route.params)
   })
 })
 </script>
